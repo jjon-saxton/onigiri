de369f270a304aa7ef36d844dd165aae41053911
Jon Saxton <jjonsaxton@gmail.com>
2023-09-05 18:00:04 -0700

    build: 2023-09-05

---
+++ public/projects.php
@@ -0,0 +1,21 @@
+<?php
+
+$projects=array();
+$folders=scandir("/home");
+
+foreach ($folders as $item)
+{
+	if (is_dir("/home/".$item."/public_html") && file_exists("/home/".$item."/public_html/MANIFEST"))
+	{
+		$info=parse_ini_file("./".$item."/MANIFEST");
+		$info['folder']=$item;
+		if (!$info['private'])
+		{
+			$projects[]=$info;
+		}
+	}
+}
+
+header("Content-type: text/json");
+header("Access-Control-Allow-Origin: http://localhost:1313");
+print json_encode($projects);
